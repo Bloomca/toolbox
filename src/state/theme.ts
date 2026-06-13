845d095878bbfc3817aca4043fdbc65a7cd4849e
Seva Zaikov <seva.zaikov@gmail.com>
2026-06-13 13:13:37 -0700

    put more colours into a theme

--- src/state/theme.ts
+++ src/state/theme.ts
@@ -3,6 +3,8 @@ import { createState } from "veles";
 type Theme = {
   id: string;
   backgroundColour: string;
+  iconSelectColour: string;
+  windowBackgroundColour: string;
 };
 
 type State = {
@@ -12,6 +14,8 @@ type State = {
 const defaultTheme: Theme = {
   id: "light",
   backgroundColour: "#b9cce9",
+  iconSelectColour: "#6161b8",
+  windowBackgroundColour: "rgb(180 200 50 / 37%)",
 };
 
 export const themeState$ = createState<State>({
@@ -20,4 +24,9 @@ export const themeState$ = createState<State>({
 
 export function applyTheme(theme: Theme) {
   document.documentElement.style.setProperty("--bg-colour", theme.backgroundColour);
+  document.documentElement.style.setProperty("--icon-select-colour", theme.iconSelectColour);
+  document.documentElement.style.setProperty(
+    "--window-background-colour",
+    theme.windowBackgroundColour,
+  );
 }
