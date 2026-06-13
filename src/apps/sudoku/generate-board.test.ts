79efbfe9f711f12e472485e39ac89a1fbeadf07a
Seva Zaikov <seva.zaikov@gmail.com>
2026-06-13 15:50:23 -0700

    update tests to validate the generated board

--- src/apps/sudoku/generate-board.test.ts
+++ src/apps/sudoku/generate-board.test.ts
@@ -2,12 +2,14 @@ import { expect, test, describe } from "vitest";
 
 import { generateBoard } from "./generate-board";
 
-// validates both rows and columns
-function validateRow(row: number[]): boolean {
-  if (row.length !== 9) return false;
+const testRuns = Array.from({ length: 100 }, (_, index) => index + 1);
+
+// validates rows, columns and squares
+function validateGroup(group: number[]): boolean {
+  if (group.length !== 9) return false;
   const dict = new Set<number>();
 
-  for (const value of row) {
+  for (const value of group) {
     if (dict.has(value)) return false;
     dict.add(value);
   }
@@ -16,20 +18,38 @@ function validateRow(row: number[]): boolean {
 }
 
 describe("generate board", () => {
-  test("all rows are correct", () => {
+  test.each(testRuns)("all rows are correct %#", () => {
     const board = generateBoard();
 
     for (const row of board) {
-      expect(validateRow(row)).toBe(true);
+      expect(validateGroup(row)).toBe(true);
     }
   });
 
-  test("all columns are correct", () => {
+  test.each(testRuns)("all columns are correct %#", () => {
     const board = generateBoard();
 
     for (let i = 0; i < 9; i++) {
       const column = board.map((row) => row[i]);
-      expect(validateRow(column)).toBe(true);
+      expect(validateGroup(column)).toBe(true);
+    }
+  });
+
+  test.each(testRuns)("all squares are correct %#", () => {
+    const board = generateBoard();
+
+    for (let rowStart = 0; rowStart < 9; rowStart += 3) {
+      for (let colStart = 0; colStart < 9; colStart += 3) {
+        const square: number[] = [];
+
+        for (let row = rowStart; row < rowStart + 3; row++) {
+          for (let col = colStart; col < colStart + 3; col++) {
+            square.push(board[row][col]);
+          }
+        }
+
+        expect(validateGroup(square)).toBe(true);
+      }
     }
   });
 });
